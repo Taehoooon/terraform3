25ababc5d7dfbd87423cbdb765b18acd3732090c
Taehoooon <67680181+Taehoooon@users.noreply.github.com>
2021-11-25 17:18:40 +0900

    Update 06_natgw.tf

--- 06_natgw.tf
+++ 06_natgw.tf
@@ -2,7 +2,7 @@ resource "aws_eip" "kim_ngw_ip" {
     vpc = true
 }
 
-resource "aws_nat_gateway" "kim_gw" {
+resource "aws_nat_gateway" "kim_ngw" {
     allocation_id = aws_eip.kim_ngw_ip.id
     subnet_id = aws_subnet.kim_puba.id
     tags = {
