c13a212b526444202a09221da25309f7b3cefecb
thkim <0503kth@gmail.com>
2021-11-26 17:50:59 +0900

    step2

--- 17_atsg.tf
+++ 17_atsg.tf
@@ -14,4 +14,6 @@ resource "aws_autoscaling_group" "kim_atsg" {
     launch_configuration = aws_launch_configuration.kim_lacf.name
     #placement_group = aws_placement_group.kim_pg.id
     vpc_zone_identifier = [aws_subnet.kim_puba.id, aws_subnet.kim_pubc.id]
+    # 이거하며 lb에서 drain 안됨
+    #target_group_arns = [aws_target_group.kim_albtg.arn]
 }
