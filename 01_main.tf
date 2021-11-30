1e9919efe96f3f286cff660d3a68399d3ea0812f
thkim <0503kth@gmail.com>
2021-11-30 09:36:02 +0900

    step

--- 01_main.tf
+++ 01_main.tf
@@ -1,12 +1,48 @@
 provider "aws" {
-    region = "ap-northeast-2"
+    region = var.region
+}
+
+resource "aws_key_pair" "kim_key" {
+    key_name = var.key
+    public_key = file("../../.ssh/kim-key.pub")
 }
 
 resource "aws_vpc" "kim_vpc" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.cidr_main
     enable_dns_hostnames = true
     enable_dns_support = true
     tags = {
-        "Name" = "kim-vpc"
+        "Name" = "${var.name}-vpc"
+    }
+}
+
+# 가용영역 a의 public subent
+resource "aws_subnet" "kim_pub" {
+    count = length(var.pub_sub)   #2
+    vpc_id = aws_vpc.kim_vpc.id
+    cidr_block = var.pub_sub[count.index] # index 0 => 10.0.0.0/24
+    availability_zone = "${var.region}${var.ava_zone[count.index]}"
+    tags = {
+     "Name" = "${var.name}-pub${var.ava_zone[count.index]}"
+    }
+}
+
+resource "aws_subnet" "kim_pri" {
+    count = length(var.pri_sub)   #2
+    vpc_id = aws_vpc.kim_vpc.id
+    cidr_block = var.pri_sub[count.index]
+    availability_zone = "${var.region}${var.ava_zone[count.index]}"
+    tags = {
+      "Name" = "${var.name}-pri${var.ava_zone[count.index]}"
+    }
+}
+
+resource "aws_subnet" "kim_pridb" {
+    count = length(var.pri_subdb)   #2
+    vpc_id = aws_vpc.kim_vpc.id
+    cidr_block = var.pri_subdb[count.index]
+    availability_zone = "${var.region}${var.ava_zone[count.index]}"
+    tags = {
+      "Name" = "${var.name}-pridb${var.ava_zone[count.index]}"
     }
 }
